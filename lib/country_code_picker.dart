f7f3e432212992fc31d08705fdc24a9f6f7598be
khaled <khaedmo@gmail.com>
2023-10-05 13:28:32 +0300

    commit

--- lib/country_code_picker.dart
+++ lib/country_code_picker.dart
@@ -212,8 +212,8 @@ class CountryCodePickerState extends State<CountryCodePicker> {
                   fit: widget.alignLeft ? FlexFit.tight : FlexFit.loose,
                   child: Padding(
                       padding: widget.alignLeft
-                          ? const EdgeInsets.only(right: 16.0, left: 8.0)
-                          : const EdgeInsets.only(right: 16.0),
+                          ? const EdgeInsets.only(right: 0.0, left: 0.0)
+                          : const EdgeInsets.only(right: 0.0),
                       child: Icon(
                         Icons.arrow_drop_down,
                         color: Colors.grey,
